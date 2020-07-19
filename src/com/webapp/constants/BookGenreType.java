cbbc80fb108cb086980099ce039de2ac6c907a5e
santoshpavan <santoshpavan.psp@gmail.com>
2020-07-19 16:14:51 -0400

    Constants classes

---
+++ src/com/webapp/constants/BookGenreType.java
@@ -0,0 +1,16 @@
+package com.webapp.constants;
+
+public class BookGenreType {
+    private BookGenreType() {}
+
+    public static final String ART = "Art";
+    public static final String BIOGRAPHY = "Biography";
+    public static final String CHILDREN = "Children";
+    public static final String FICTION = "Fiction";
+    public static final String HISTORY = "History";
+    public static final String MYSTERY = "Mystery";
+    public static final String PHILOSOPHY = "Philosophy";
+    public static final String RELIGION = "Religion";
+    public static final String SELF_HELP = "Self Help";
+    public static final String TECHNICAL = "Technical";
+}
---
+++ src/com/webapp/constants/GenderType.java
@@ -0,0 +1,9 @@
+package com.webapp.constants;
+
+public class GenderType {
+    private GenderType() {}
+
+    private static final int MALE = 1;
+    private static final int FEMALE = 2;
+    private static final int TRANSGENDER = 3;
+}

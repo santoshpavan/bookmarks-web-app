407b0f8f129a656da7b12b07f433088c837189e4
santoshpavan <santoshpavan.psp@gmail.com>
2020-07-19 21:41:16 -0400

    Launching application - main method

---
+++ src/com/webapp/Launch.java
@@ -0,0 +1,41 @@
+package com.webapp;
+
+import com.webapp.entities.Bookmark;
+import com.webapp.entities.User;
+import com.webapp.managers.BookmarkManager;
+import com.webapp.managers.UserManager;
+
+public class Launch {
+    private static User[] users;
+    private static Bookmark[][] bookmarks;
+
+    public static void loadData() {
+        System.out.println("Loading data...");
+        DataStore.loadData();
+
+        users = UserManager.getInstance().getUsers();
+        bookmarks = BookmarkManager.getInstance().getBookmarks();
+
+        System.out.println("Printing data...");
+        printUserData();
+        printBookmarkData();
+    }
+
+    public static void printUserData() {
+        for(User user: users) {
+            System.out.println(user);
+        }
+    }
+
+    public static void printBookmarkData() {
+        for(Bookmark[] bookmarksList: bookmarks) {
+            for(Bookmark bookmark: bookmarksList) {
+                System.out.println(bookmark);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        loadData();
+    }
+}
